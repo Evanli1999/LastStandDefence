5bb5b8250e412cb4ad64d31aa8f2bccdf7050e41
ThunderBird2678 <edwardthetrueblade2678@gmail.com>
2016-06-13 13:53:57 -0400

    Adding and Removing Towers
    These are implemented graphically now. Yay for me. Commit this if you want, Mitro, but I'm going to revamp the DrawArea class as soon as I get home.

--- GameBoard.java
+++ GameBoard.java
@@ -7,30 +7,28 @@ public class GameBoard extends JFrame
   
   // ArrayList<Square> squares;
   
-  protected MiscImage wavesImage =           new MiscImage(40 , 160, 500, 70, "resources//backings//waves.png");
-  protected MiscImage killsImage =           new MiscImage(240, 160, 500, 70, "resources//backings//kills.png");
+  protected MiscImage wavesImage =           new MiscImage(40 , 160, 500, 70, "resources/backings/waves.png");
+  protected MiscImage killsImage =           new MiscImage(240, 160, 500, 70, "resources/backings/kills.png");
   // protected MiscImage centerImage =          new MiscImage(460, 160, 500, 70, "resources//backings//center.png"); Now that I think about it, this isn't actually useful
-  protected MiscImage nextWaveImage =        new MiscImage(680, 160, 500, 70, "resources//backings//nextWave.png");
-  protected MiscImage pauseImage =           new MiscImage(880, 160, 500, 70, "resources//backings//pause.png");
+  protected MiscImage nextWaveImage =        new MiscImage(680, 160, 500, 70, "resources/backings/nextWave.png");
+  protected MiscImage pauseImage =           new MiscImage(880, 160, 500, 70, "resources/backings/pause.png");
+  protected MiscImage backgroundImage =      new MiscImage(0 , 1080, 0 , 600, "resources/overall/background.png");
+  protected MiscImage boardBackgroundImage = new MiscImage(40, 1000, 40, 440, "resources/overall/boardBackground.png"); // This is actually not useless, it's used for a bit of checking, will clean up soon(tm)
   
-  protected MiscImage backgroundImage =      new MiscImage(0 , 1080, 0 , 600, "resources//overall//background.png");
-  protected MiscImage boardBackgroundImage = new MiscImage(40, 1000, 40, 440, "resources//overall//boardBackground.png"); // This is actually not useless, it's used for a bit of checking, will clean up soon(tm)
+  protected MiscImage baseImage =            new MiscImage(840, 160, 265, 70, "resources/entities/base.png"); // These coordinates can be changed depending on how we want to set up the map
   
-  protected MiscImage baseImage =            new MiscImage(840, 160, 265, 70, "resources//entities//base.png"); // These coordinates can be changed depending on how we want to set up the map
+  protected MiscImage currentTurretImage =   new MiscImage(524, 40, 518, 40, "resources/turrets/blue.png"); // I'll deal with this one later
+  protected MiscImage buttonsImage =         new MiscImage(478, 125, 520, 30, "resources/controls/arrows.png"); // I'll deal with this one later
   
-  protected MiscImage currentTurretImage =   new MiscImage(524, 40, 518, 40, "resources//turrets//blue.png"); // I'll deal with this one later
-  protected MiscImage buttonsImage =         new MiscImage(478, 125, 520, 30, "resources//controls//arrows.png"); // I'll deal with this one later
-  
-  protected MiscImage path =                 new MiscImage(40, 880, 80, 360, "resources//entities//path.png");
+  protected MiscImage path =                 new MiscImage(40, 880, 80, 360, "resources/entities/path.png");
   
   // protected ArrayList<MiscImage> path;    This is placeholder at the moment, the path will be procedurally generated based upon the coordinates I get from the other logic classes, right now I'm hardcoding a path
-  protected ArrayList<MiscImage> turrets;
+  protected ArrayList<MiscImage> turrets = new ArrayList<MiscImage>();
   // protected ArrayList<Square> squares; Currently commented off because I don't have the squares class yet
   
   protected DrawArea main; // This is the main area where all of our components will be drawn onto.
   
-  protected int squareX;
-  protected int squareY;
+  protected Position select = new Position (0,0); //if you want to access this in a static method, use Gameboard.aboard.select
   
   // Okay, for now, I'm defining each square to be 40px by 40px
   
@@ -56,19 +54,69 @@ public class GameBoard extends JFrame
     main.add(buttonsImage);
     main.add(currentTurretImage);
     
+    main.add(turrets);
+    
     setContentPane(main);                                // set the content pane to be whatever content pane contains all the others
     pack ();                                             // this is apparently required
     setTitle ("GUI Testing");                            // set the title of the window
     setSize (1080, 630);                                 // set the size of the window (in pixels)
     setDefaultCloseOperation (JFrame.EXIT_ON_CLOSE);     // set the close operation (just use EXIT_ON_CLOSE, we're not one of those dicks who minimizes windows when the user hits close)
     setLocationRelativeTo (null);                        // Center window.
-     
+    
   }
   
-  public static void main (String [] args)
+  public void revert()
   {
-    GameBoard aBoard = new GameBoard();
-    aBoard.setVisible(true);
+    main.revert();
+    
+    main.add(backgroundImage);
+    main.add(wavesImage);
+    main.add(killsImage);
+    main.add(nextWaveImage);
+    main.add(pauseImage);
+    main.add(baseImage);
+    main.add(buttonsImage);
+    main.add(currentTurretImage);
+    
+    main.add(turrets);
+  }
+  
+  public void addTower(Tower toAdd)
+  {
+    int towerX = toAdd.getPos().getX();
+    int towerY = toAdd.getPos().getY();
+    System.out.println("Tower is located at: (" + towerX + ", " + towerY + ")");
+    
+    int towerDrawX = (towerX * 40) + 40;
+    int towerDrawY = (towerY * 40) + 40;
+    
+    turrets.add(new MiscImage(towerDrawX, 40, towerDrawY, 40, "resources\\turrets\\blue.png"));
+    
+    revert();
+    main.add(turrets);
+    repaint();
+    
+  }
+  
+  public void removeTower(Tower toRemove)
+  {
+    int towerX = toRemove.getPos().getX();
+    int towerY = toRemove.getPos().getY();
+    System.out.println("Removing tower at: (" + towerX + ", " + towerY + ")");
+    int towerDrawX = (towerX * 40) + 40;
+    int towerDrawY = (towerY * 40) + 40;
+    
+    for(int i = 0; i < turrets.size(); i++)
+    {
+      if(turrets.get(i).getX() == towerDrawX && turrets.get(i).getY() == towerDrawY)
+      {
+        turrets.remove(i);
+      }
+    }
+   
+    revert();
+    repaint();
+    
   }
   
   class msListen implements MouseListener, MouseMotionListener
@@ -88,15 +136,13 @@ public class GameBoard extends JFrame
         int calcX = e.getX() - 40;
         int calcY = e.getY() - 40;
         
-        squareX = calcX / 40;
-        squareY = calcY / 40;
-        
-        System.out.println("Square coordinates: (" + squareX + ", " + squareY + ")");
+        select.setX(calcX / 40);
+        select.setY(calcY / 40);
+        System.out.println("Selected coordinates: "+select);
         
       }
       
-      
-      System.out.println("(" + e.getX() + ", " + e.getY() + ")");
+      System.out.println("(MouseClick at" + e.getX() + ", " + e.getY() + ")");
     }
     
     public void mouseMoved(MouseEvent e)
--- LSD.java
+++ LSD.java
@@ -1,32 +1,48 @@
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class LSD
 {
-  protected int money = 2000;
-  protected int score = 0;
-  ArrayList<Tower> towers = new ArrayList(0);
-  ArrayList<Position> path = new ArrayList(0);
+  protected static int money = 2000;
+  protected static int score = 0;
+  static GameBoard aBoard = new GameBoard();
+  static ArrayList<Tower> towers = new ArrayList(0);
+  static ArrayList<Position> path = new ArrayList(0);
   protected static ArrayList<Enemy> wave = new ArrayList(0);
   
   public static void main(String args[])
   {
-  
+    System.out.println("LSD v.0.01 Codename 'Incompetence'\npress return to add, enter any string then return to remove.");
+    aBoard.setVisible(true);
+    Scanner little = new Scanner(System.in);
+    while(true)
+    {
+      String inputs = little.nextLine();
+      System.out.println("current position: "+aBoard.select);
+      if(inputs .isEmpty())
+        addTower(aBoard.select);
+      else
+        removeTower(aBoard.select);
+    }
   }
   
   public static boolean canBuild(Position place)
   {
     for(int a = 0;a<path.size();a++)
     {
-      if(path.get(0).distance(place)==0)//if place is on the path
+      if(path.get(a).distance(place)==0)//if place is on the path
       {
+        System.out.println(place+" is part of the path. Can't build.");
         return false; //no building allowed
       }
     }
     
     for(int a = 0;a<towers.size();a++)
     {
+      //System.out.println("Tower "+a+": "+towers.get(a).getPos()+towers.get(a).getPos().distance(place));
       if(towers.get(a).getPos().distance(place)==0)//tower is at that position
       {
+        System.out.println("There is a tower at "+place+" Can't build.");
         return false; //no building allowed
       }
     }
@@ -40,20 +56,42 @@ public class LSD
     {
       if(money>200)//change this to a proper requirement
       {
-        towers.add(new Tower());
+        System.out.println("Added tower at "+place);
+        
+        Tower toAdd = new Tower(0, new Position(place)); 
+        towers.add(toAdd);
+        
+        money-=200; //subtract funds
+        aBoard.addTower(toAdd);
         //then update graphics as necessary
+      }
+      else
+      {
+        System.out.println("Insufficient funds.");
       }
     }
   }
   
   public static void removeTower(Position place)
   {
-    for(int a = 0;a<towers.size();a++)
+    for(int a = 0;a<=towers.size();a++)
     {
+      if(a==towers.size())
+      {
+        System.out.println("There is no tower there.");
+      }
+      
       if(towers.get(a).getPos().distance(place)==0)
       {
+        System.out.println("Removed tower "+a+" from "+towers.get(a).getPos());
+        
+        Tower toRemove = new Tower(0, new Position(place)); 
         towers.remove(a);
+        
+        aBoard.removeTower(toRemove);
+        
         //then update graphics as necessary
+        break;
       }
     }
   }
