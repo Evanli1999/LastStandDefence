cfa2985104be23fbcffc288bb172c64b38abb5da
ShinyLuxray <3point14159265358@gmail.com>
2016-06-13 23:55:11 -0400

    Updating main and Position classes
    Load from paths.txt the paths
    Added a neat parser method in main
    Added String constructor for position

--- LSD.java
+++ LSD.java
@@ -1,127 +1,155 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Scanner;
 
 public class LSD
 {
-  protected static int money = 2000;
-  protected static int score = 0;
-  
-  static ArrayList<Position> path = new ArrayList<Position>();
-  
-  
-  static GameBoard aBoard;
-  static ArrayList<Tower> towers = new ArrayList(0);
-  protected static ArrayList<Enemy> wave = new ArrayList(0);
-  
-  public static void main(String args[])
-  {
-    
-    path.add(new Position(0, 4));
-    path.add(new Position(1, 4));
-    path.add(new Position(2, 4));
-    path.add(new Position(3, 4));
-    path.add(new Position(4, 4));
-    path.add(new Position(5, 4));
-    path.add(new Position(6, 4));
-    path.add(new Position(7, 4));
-    path.add(new Position(8, 4));
-    path.add(new Position(9, 4));
-    path.add(new Position(10, 4));
-    path.add(new Position(11, 4));
-    path.add(new Position(12, 4));
-    path.add(new Position(13, 4));
-    path.add(new Position(13, 5));
-    path.add(new Position(13, 6));
-    path.add(new Position(14, 6));
-    path.add(new Position(15, 6));
-    path.add(new Position(16, 6));
-    path.add(new Position(17, 6));
-    path.add(new Position(18, 6));
-    path.add(new Position(19, 6));
-    
-    aBoard = new GameBoard(path);
-    
-    System.out.println("LSD v.0.01 Codename 'Incompetence'\npress return to add, enter any string then return to remove.");
-    aBoard.setVisible(true);
-    Scanner little = new Scanner(System.in);
-    while(true)
+    protected static int money = 2000;
+    protected static int score = 0;
+    static GameBoard aBoard = new GameBoard();
+    static ArrayList<Tower> towers = new ArrayList(0);
+    static ArrayList<Position> path = new ArrayList(0);
+    protected static ArrayList<Enemy> wave = new ArrayList(0);
+
+    public static void main(String args[])
     {
-      String inputs = little.nextLine();
-      System.out.println("current position: "+aBoard.select);
-      if(inputs .isEmpty())
-        addTower(aBoard.select);
-      else
-        removeTower(aBoard.select);
+        System.out.println("LSD v.0.01 Codename 'Incompetence'\npress return to add, enter any string then return to remove.");
+        aBoard.setVisible(true);
+        try
+        {
+           ArrayList<String> temp = parse(parse(new String(Files.readAllBytes(Paths.get("paths.txt"))),'\n').get(0),'#');
+           
+           for(int a = 0;a<temp.size();a++)
+           {
+               path.add(new Position(temp.get(a)));
+           }
+        }
+        catch(IOException e)
+        {
+            System.out.println("Could not read file.");
+        }
+        Scanner little = new Scanner(System.in);
+        while(true)
+        {
+            String inputs = little.nextLine();
+            System.out.println("current position: "+aBoard.select);
+            if(inputs .isEmpty())
+                addTower(aBoard.select);
+            else
+                removeTower(aBoard.select);
+        }
     }
-  }
-  
-  public static boolean canBuild(Position place)
-  {
-    for(int a = 0;a<path.size();a++)
+
+    public static boolean canBuild(Position place)
     {
-      if(path.get(a).distance(place)==0)//if place is on the path
-      {
-        System.out.println(place+" is part of the path. Can't build.");
-        return false; //no building allowed
-      }
+        for(int a = 0;a<path.size();a++)
+        {
+            if(path.get(a).distance(place)==0)//if place is on the path
+            {
+                System.out.println(place+" is part of the path. Can't build.");
+                return false; //no building allowed
+            }
+        }
+
+        for(int a = 0;a<towers.size();a++)
+        {
+            //System.out.println("Tower "+a+": "+towers.get(a).getPos()+towers.get(a).getPos().distance(place));
+            if(towers.get(a).getPos().distance(place)==0)//tower is at that position
+            {
+                System.out.println("There is a tower at "+place+" Can't build.");
+                return false; //no building allowed
+            }
+        }
+
+        return true;
     }
-    
-    for(int a = 0;a<towers.size();a++)
+
+    public static void addTower(Position place)//a flimsy addTower class, makes a default Tower()
     {
-      //System.out.println("Tower "+a+": "+towers.get(a).getPos()+towers.get(a).getPos().distance(place));
-      if(towers.get(a).getPos().distance(place)==0)//tower is at that position
-      {
-        System.out.println("There is a tower at "+place+" Can't build.");
-        return false; //no building allowed
-      }
+        if(canBuild(place))
+        {
+            if(money>200)//change this to a proper requirement
+            {
+                System.out.println("Added tower at "+place);
+                towers.add(new Tower(0,new Position(place)));
+                money-=200; //subtract funds
+                //then update graphics as necessary
+            }
+            else
+            {
+                System.out.println("Insufficient funds.");
+            }
+        }
     }
-    
-    return true;
-  }
-  
-  public static void addTower(Position place)//a flimsy addTower class, makes a default Tower()
-  {
-    if(canBuild(place))
+
+    public static void removeTower(Position place)
     {
-      if(money>200)//change this to a proper requirement
-      {
-        System.out.println("Added tower at "+place);
-        
-        Tower toAdd = new Tower(0, new Position(place)); 
-        towers.add(toAdd);
-        
-        money-=200; //subtract funds
-        aBoard.addTower(toAdd);
-        //then update graphics as necessary
-      }
-      else
-      {
-        System.out.println("Insufficient funds.");
-      }
+        for(int a = 0;a<=towers.size();a++)
+        {
+            if(a==towers.size())
+            {
+                System.out.println("There is no tower there.");
+                break;
+            }
+            if(towers.get(a).getPos().distance(place)==0)
+            {
+                System.out.println("Removed tower "+a+" from "+towers.get(a).getPos());
+                towers.remove(a);
+                break;
+                //then update graphics as necessary
+            }
+        }
     }
-  }
-  
-  public static void removeTower(Position place)
-  {
-    for(int a = 0;a<=towers.size();a++)
+
+    public static ArrayList<String> parse(String parseThis,char delimiter)
     {
-      if(a==towers.size())
-      {
-        System.out.println("There is no tower there.");
-      }
-      
-      if(towers.get(a).getPos().distance(place)==0)
-      {
-        System.out.println("Removed tower "+a+" from "+towers.get(a).getPos());
-        
-        Tower toRemove = new Tower(0, new Position(place)); 
-        towers.remove(a);
-        
-        aBoard.removeTower(toRemove);
-        
-        //then update graphics as necessary
-        break;
-      }
+        int width = 0,elements = 0,temp = 0;
+
+        for(int a = 0; a < parseThis.length();a++)//check the first row to see what the dimension should be
+        {
+            if(parseThis.charAt(a) == ',')
+            {
+                width++;
+            }
+        }
+
+        ArrayList<String> toR = new ArrayList(0);
+
+        if(width==0)
+        {
+	    toR.add(parseThis);
+            return toR;
+        }
+
+        for(int b = 0;elements<width;b++)
+        {
+            if(elements>=width-1)//if last element copy the rest
+            {
+                toR.add(parseThis.substring(temp));
+                break;
+            }
+
+            if(b>=parseThis.length())//if we are at the end of the line before all the elements filled
+            {
+                while(elements<width)
+                {
+                    toR.add("");//just fill it with blank then
+                    elements++;
+                }
+
+                break;
+            }
+
+            if(parseThis.charAt(b)==delimiter)//delimiter is the comma
+            {
+                toR.add(parseThis.substring(temp,b));//set the former stuff to the element
+                temp = b+1;//shorten the string we have to work with
+                elements++;//go to next element
+            }
+        }
+
+        return toR;
     }
-  }
 }
